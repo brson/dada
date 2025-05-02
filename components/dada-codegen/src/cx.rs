9f23714d62b90523707c0fb96bd657be74d8e782
Brian Anderson <andersrb@gmail.com>
2025-05-02 14:52:18 -0600

    .

--- components/dada-codegen/src/cx.rs
+++ components/dada-codegen/src/cx.rs
@@ -2,6 +2,9 @@ use dada_ir_sym::{ir::functions::SymFunction, ir::types::SymGenericTerm};
 use dada_util::{FromImpls, Map};
 use salsa::Update;
 use wasm_encoder::{CodeSection, ExportKind, ExportSection, FunctionSection, TypeSection};
+use wasm_encoder::{
+    ComponentTypeSection,
+};
 
 mod generate_expr;
 mod generate_fn;
@@ -17,6 +20,8 @@ pub(crate) struct Cx<'db> {
     export_section: ExportSection,
     functions: Map<FnKey<'db>, FnIndex>,
     codegen_queue: Vec<CodegenQueueItem<'db>>,
+
+
 }
 
 impl<'db> Cx<'db> {
@@ -38,6 +43,30 @@ impl<'db> Cx<'db> {
         function: SymFunction<'db>,
         generics: Vec<SymGenericTerm<'db>>,
     ) -> wasm_encoder::Module {
+        let index = self.declare_fn(function, generics);
+        while let Some(item) = self.codegen_queue.pop() {
+            match item {
+                CodegenQueueItem::Function(fn_key) => self.codegen_fn(fn_key),
+            }
+        }
+
+        let name = function.name(self.db).text(self.db);
+        self.export_section.export(name, ExportKind::Func, index.0);
+
+        let mut module = wasm_encoder::Module::new();
+        module.section(&self.type_section);
+        module.section(&self.function_section);
+        module.section(&self.export_section);
+        module.section(&self.code_section);
+
+        module
+    }
+
+    pub fn generate_component_from_fn(
+        mut self,
+        function: SymFunction<'db>,
+        generics: Vec<SymGenericTerm<'db>>,
+    ) -> wasm_encoder::Component {
         let index = self.declare_fn(function, generics);
         while let Some(item) = self.codegen_queue.pop() {
             match item {
